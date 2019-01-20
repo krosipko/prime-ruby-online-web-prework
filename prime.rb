fa1fb6ce91447123d226d3f8d64fd2c1d9262a82
Kyle Rosipko <krosipko@outlook.com>
2019-01-20 00:50:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(argument)
+  count = 2 
+  div = []
+  while count < argument
+    if argument % count == 0 
+      div << count
+    end
+    test = argument % count 
+    count += 1
+  end
+  if div.length > 0 
+    false 
+  elsif argument <= 0 || 1
+    false 
+  else
+    true 
+  end
+  
+end
